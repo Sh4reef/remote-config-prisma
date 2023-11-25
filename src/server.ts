2091253f2d018553fc52a416286b623ccf685ced
Shareef <shareef.banjar@gmail.com>
2023-11-25 10:41:04 +0700

    Server configuration - Use express instead of standalone server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import { ApolloServer } from "@apollo/server";
-import {
-  startStandaloneServer,
-} from "@apollo/server/standalone";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import schema from "./schema";
 import createContext, { Context } from "./context";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
+const app = express();
+const httpServer = http.createServer(app);
 const apolloServer = new ApolloServer<Context>({
   schema,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   formatError(formattedError) {
     return {
       message: formattedError.message,
@@ -16,11 +23,27 @@ const apolloServer = new ApolloServer<Context>({
 
 (async () => {
   try {
-    await startStandaloneServer(apolloServer, {
-      context: createContext,
+    await apolloServer.start();
+
+    app.use(
+      "/graphql",
+      cors<cors.CorsRequest>(),
+      express.json(),
+      expressMiddleware(apolloServer, {
+        context: createContext,
+      })
+    );
+
+    app.get("/identities", async (req, res) => {
+      const identities = await prisma.identity.findMany();
+      return res.json(identities);
     });
 
-    console.log("URL: http://localhost:4000");
+    await new Promise<void>((resolve) =>
+      httpServer.listen({ port: 4000 }, resolve)
+    );
+
+    console.log(`🚀 Server ready at http://localhost:4000/graphql`);
   } catch (error) {
     console.log(error);
   }
